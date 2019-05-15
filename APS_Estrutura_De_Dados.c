77fe3826d99854becbc73b7a1d7f67b172e3d4e6
SUPREME DEVELOPER <conradopsa@gmail.com>
2019-05-15 17:10:06 -0300

    Add files via upload
    Função contadora de folhas implementada!

--- APS_Estrutura_De_Dados.c
+++ APS_Estrutura_De_Dados.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/* 
+	Faça um programa em C que crie uma árvore binária e implemente uma função
+	que retorne a quantidade de folhas de uma árvore binária.
+	
+	Essa função deve obedecer ao protótipo:
+	int folhas (Arv* a); 
+*/
+
 typedef struct noArv{
 	int valor;
 	struct noArv* esq;
@@ -27,27 +35,58 @@ noArv* cria_arvore(int valor, noArv* esquerda, noArv* direita){
 	return raiz;
 }
 
+//Contador de folhas (nós que não possuem filhos)
+int auxCont;
+void Percorre(noArv* noAux){
 
-int folhas(noArv* raiz){
-	
+	//Imprimindo Nós
+	printf("%d ", noAux->valor);
 
+	if (noAux->esq == NULL && noAux->dir == NULL)
+		auxCont++;
 
-	
-	
+	if (noAux->esq != NULL)
+		Percorre(noAux->esq);
+
+	if (noAux->dir != NULL)
+		Percorre(noAux->dir);
+
 }
+
+int folhas(noArv* noAux){
+	auxCont = 0;
+	Percorre(noAux);
+	return auxCont;
+}
+//Fim do Contador
 
 int main() {
 
-	//Criando a avore...
-	noArv* a1 = cria_arvore(20, cria_no(), cria_no());
-	noArv* a2 = cria_arvore(40, cria_no(), cria_no());
-	noArv* a3 = cria_arvore(20, a1, a2);
-	noArv* a4 = cria_arvore(70, cria_no(), cria_no());
-	noArv* a5 = cria_arvore(60, cria_no(), a4);
-	noArv* raiz = cria_arvore(50, a3, a5);
+	/*
+		Exemplo:
+		      50
+			/    \
+		   20     60
+		  /  \      \
+		40    80     10
+		
+		Genéricamente:
+			   A
+			 /   \
+			B     C
+		   / \     \
+		  D   E     F
+	*/
+
+	//Criando a arvore...    
+	noArv* F = cria_arvore(10, NULL, NULL);
+	noArv* E = cria_arvore(80, NULL, NULL);
+	noArv* D = cria_arvore(40, NULL, NULL);
+    noArv* C = cria_arvore(60, NULL, F);
+	noArv* B = cria_arvore(20, D, E);
+	noArv* A = cria_arvore(50, B, C); //Raíz
 	
-	printf(" %d ", folhas(raiz));
-	
-	
+	printf("\nQuantidade de Folhas = %d\n", folhas(A));	
+
 	return 0;
 }
